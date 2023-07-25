cd4de0e69d779aefa9fc1e1095955d233eb43032
NikkiTallerThanYou <99840838+NikkiTallerThanYou@users.noreply.github.com>
2023-07-25 01:36:45 -0400

    Swerve module skeleton

--- Swerve-Drive/src/main/java/frc/robot/Swerve/SwerveModule.java
+++ Swerve-Drive/src/main/java/frc/robot/Swerve/SwerveModule.java
@@ -2,6 +2,20 @@ package frc.robot.Swerve;
 
 
 public class SwerveModule {
+    
+    //get abs angle
 
-//go crazy rahhhh go stupid aggahhgahghg
+    //optimize turning
+
+    //set desired state
+
+    //setters + getters for speed + theta (separately)
+
+    //get drive motor direction
+
+    //get motor position
+
+    //config motor offset (in the case the motor and encoder are not aligned)
+
+    //zero out drive motor
 }
