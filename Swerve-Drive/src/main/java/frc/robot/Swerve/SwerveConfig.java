f6ecfd2cce352579f9185871f5564e0f28b27a84
NikkiTallerThanYou <99840838+NikkiTallerThanYou@users.noreply.github.com>
2023-11-08 16:37:49 -0500

    More bare bones code of swerve

--- Swerve-Drive/src/main/java/frc/robot/Swerve/SwerveConfig.java
+++ Swerve-Drive/src/main/java/frc/robot/Swerve/SwerveConfig.java
@@ -1,60 +1,8 @@
 package frc.robot.Swerve;
 
-public enum SwerveConfig{
+public class SwerveConfig{
 
-    //TODO: discover what the wheel reduction actually is
-    SwerveX(
-        0.1524,
-        1,
-        true,
-        1,
-        true
-    );
 
-    private final double wheelDiameter;
-    private final double driveReduction;
-    private final boolean driveInverted;
-  
-    private final double steerReduction;
-    private final boolean steerInverted;
     
-    /* 
-    * 
-    * @param wheelDiameter: the diameter of the wheel (meters)
-    * 
-    * @param driveReduction: the reduction of the drive motor
-    * 
-    * @param driveInverted: whether the drive motor is inverted or not
-    * 
-    * @param steerReduction: the reduction of the steer motor
-    *  
-    * @param steerInverted: whether the steer motor is inverted or not. If there are an odd number of reductions
-    *                       then this should be true
-     */
-
-    private SwerveConfig(double wheelDiameter, double driveReduction, boolean driveInverted, 
-        double steerReduction, boolean steerInverted) {
-     this.wheelDiameter = wheelDiameter;
-     this.driveReduction = driveReduction;
-     this.driveInverted = driveInverted;
-     this.steerReduction = steerReduction;
-     this.steerInverted = steerInverted;
-    }
-    
-    public double getWheelDiameter() {
-        return wheelDiameter;
-    }
-    public double getDriveReduction() {
-        return driveReduction;
-    }
-    public boolean getDriveInverted() {
-        return driveInverted;
-    }
-    public double getSteerReduction() {
-        return steerReduction;
-    }
-    public boolean getSteerInverted() {
-        return steerInverted;
-    }
 }
 
--- Swerve-Drive/src/main/java/frc/robot/Swerve/SwerveModule.java
+++ Swerve-Drive/src/main/java/frc/robot/Swerve/SwerveModule.java
@@ -1,13 +1,94 @@
 package frc.robot.Swerve;
 
+import com.revrobotics.AnalogInput;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.SparkMaxRelativeEncoder;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+import edu.wpi.first.math.util.Units;
 
 public class SwerveModule {
     
+    /* Motors/Encoders */
+
+    private CANSparkMax m_driveMotor;
+    private CANSparkMax m_steerMotor;
+
+    private SparkMaxRelativeEncoder m_driveEncoder;
+    private SparkMaxRelativeEncoder m_turningEncoder;
+
+    private SparkMaxPIDController turningPIDController;
+
+    //Assuming the absolute encoder is attached to the RIO--otherwise 
+    //it may just be the SparkMaxAbsoluteEncoder class if built in
+    private AnalogInput absoluteEncoder;
+
+    /* Boolean conditions in case the positions are 
+    reversed to how we want it when physically starting the bot */
+    private boolean isAbsEncoderReversed;
+    private double absoluteEncoderOffsetRad;
+
+    
+
+    /* Constants (may move later) */
+
+    final double kGearRatio = 4.71; //4.71:1 ???
+    final int kWheelDiameterInches = 3;
+
+    //Controller --> Chassispeeds --> Swerve Module State [x4]
+
     //get abs angle
 
-    //optimize turning
+    //Set desired state
+    public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop, boolean steerInPlace){
 
-    //set desired state
+
+
+    }
+
+    //optimize turning
+    public SwerveModuleState optimize(SwerveModuleState desiredState, Rotation2d currentAngle){
+        double targetAngle = orientTo2Pi(currentAngle.getDegrees(), desiredState.angle.getDegrees());
+        double targetSpeed = desiredState.speedMetersPerSecond;
+        double delta = targetAngle - currentAngle.getDegrees();
+        if(Math.abs(delta) > 90){
+            targetSpeed = -targetSpeed;
+            targetAngle = delta > 90 ? (targetAngle -= 180) : (targetAngle += 180);
+        }
+        return new SwerveModuleState(targetSpeed, Rotation2d.fromDegrees(targetAngle));
+    }
+
+
+    //orient angle
+    public double orientTo2Pi(double scopeReference, double newAngle){
+        double lowerBound;
+        double upperBound;
+        double lowerOffset = scopeReference % 360;
+
+        //Whether the angle is above a full rotation
+        if(lowerOffset >= 0){
+            lowerBound = scopeReference - lowerOffset;
+            upperBound = scopeReference + (360 - lowerOffset);
+        } else {
+            upperBound = scopeReference - lowerOffset;
+            lowerBound = scopeReference + (360 + lowerOffset);
+        }
+        while(newAngle < lowerBound){
+            newAngle += 360;
+        }
+        while(newAngle > upperBound){
+            newAngle -= 360;
+        }
+        if(newAngle - scopeReference > 180){
+            newAngle += 360;
+        }
+        return newAngle;
+    }
 
     //setters + getters for speed + theta (separately)
 
@@ -18,4 +99,32 @@ public class SwerveModule {
     //config motor offset (in the case the motor and encoder are not aligned)
 
     //zero out drive motor
+
+
+    /* Gear ratio is 4.71:1 according to the MAXSwerve Module
+     purchashing page */
+
+    public SwerveModuleState getState(){
+        return new SwerveModuleState();
+    }
+
+    public double getDrivePosition(){ 
+        return 0.0;
+    }
+    public double getTurningPosition(){ 
+        return 0.0;
+    }
+
+    public double getDriveVelocity(){ return 0.0;}
+    public double getTurningVelocity(){ return 0.0;}
+
+
+     //m/s to rpm
+      //Velocity(m/s) / wheel circumference (meters/revolution) * gear ratio (unitless) * 60 (seconds) --> dimensional analysis
+    private double VelocitytoRPM(double velocityMetersPerSecond){
+
+        return (velocityMetersPerSecond * 60 * kGearRatio) / ((Units.inchesToMeters(kWheelDiameterInches) * Math.PI));
+ 
+    }
+
 }
